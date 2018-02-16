ac11741c643a9dc6161e0b79bbdd8e5b0af7703f
Duncan Garland <duncan.garland@ntlworld.com>
2018-02-16 13:08:14 +0000

    Routes and forms. Seems to have memory leak!

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -2,22 +2,27 @@
 namespace Blog;
 
 use Zend\Router\Http\Literal;
-
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
     'service_manager' => [
         'aliases' => [
             Model\PostRepositoryInterface::class => Model\ZendDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\PostCommand::class,
+            Model\PostCommandInterface::class => Model\ZendDbSqlCommand::class,
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
             Model\ZendDbSqlRepository::class => Factory\ZendDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\ZendDbSqlCommand::class => Factory\ZendDbSqlCommandFactory::class,
         ],
     ],
     'controllers' => [
         'factories' => [
             Controller\ListController::class => Factory\ListControllerFactory::class,
+            Controller\WriteController::class => Factory\WriteControllerFactory::class
             # Model\ZendDbSqlRepository::class => Factory\ZendDbSqlRepositoryFactory::class,
         ],
     ],
@@ -38,6 +43,31 @@ return [
                     'defaults' => [
                         'controller' => Controller\ListController::class,
                         'action'     => 'index',
+                    ],
+                ],
+                'may_terminate' => true,
+                'child_routes'  => [
+                    'detail' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route'    => '/:id',
+                            'defaults' => [
+                                'action' => 'detail',
+                            ],
+                            'constraints' => [
+                                'id' => '[1-9]\d*',
+                            ],
+                        ],
+                    ],
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route'    => '/add',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'add',
+                            ],
+                        ],
                     ],
                 ],
             ],
